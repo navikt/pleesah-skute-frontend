3a21b64a792bc402a9ffaac442787629b02141ee
Tania Holst <holst.tania@gmail.com>
2026-01-13 12:11:02 +0100

    :sparkles: Init forside og oppgavesider

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {Link, Route, Routes} from 'react-router-dom'
+import {Oppgave1} from './oppgaver/oppgave_1.tsx'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const Forside = () => {
+    return (<>
+        <h1>Pleesah Game</h1>
+        <Link to="/oppgaver/1/">Start oppgave 1</Link>
+    </>)
 }
+
+const App =() => {
+    return (<>
+        <Routes>
+            <Route path="/" element={<Forside/>}/>
+            <Route path="/oppgaver/1/" element={<Oppgave1/>}/>
+        </Routes>
+    </>)
+}
+
 
 export default App
---
+++ web/src/oppgaver/oppgave_1.tsx
@@ -0,0 +1,10 @@
+export const Oppgave1 = () => {
+  return (
+    <>
+      <h1>Oppgave 1</h1>
+
+
+
+    </>
+  )
+}
